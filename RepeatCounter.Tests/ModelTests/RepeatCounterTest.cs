8f3b0647ee82e91b432395a4fed7b5a3ca5e6bff
Guest User <Guest@Epicodus-6E.local>
2018-02-16 09:07:50 -0800

    create red test condition for GetFindString test method

--- RepeatCounter.Tests/ModelTests/RepeatCounterTest.cs
+++ RepeatCounter.Tests/ModelTests/RepeatCounterTest.cs
@@ -12,10 +12,23 @@ namespace RepeatCounter.Tests
     public void GetInputString_GetsString_ReturnsString()
     {
       //arrange
-      RepeatCounterGenerator newRepeatCounterGenerator = new RepeatCounterGenerator("B");
+      RepeatCounterGenerator newRepeatCounterGenerator = new RepeatCounterGenerator("B", "C");
       string testString = "B";
       //act
       string testInput = newRepeatCounterGenerator.GetInputString();
+      //assert
+      Assert.AreEqual(testString, testInput);
+    }
+    [TestMethod]
+    public void GetFindString_GetsString_ReturnsString()
+    {
+      //arrange
+      RepeatCounterGenerator newRepeatCounterGenerator =
+      new RepeatCounterGenerator("dog", "buffalo");
+      string testString = "buffalo";
+      //act
+      string testInput = newRepeatCounterGenerator.GetFindString();
+
       //assert
       Assert.AreEqual(testString, testInput);
     }
--- RepeatCounter/Models/RepeatCounter.cs
+++ RepeatCounter/Models/RepeatCounter.cs
@@ -7,15 +7,22 @@ namespace RepeatCounter.Models
   public class RepeatCounterGenerator
   {
       private string _inputString;
+      private string _findString;
 
-      public RepeatCounterGenerator(string inputString)
+      public RepeatCounterGenerator(string inputString, string findString)
     {
       _inputString = inputString;
+      _findString = findString;
     }
 
     public string GetInputString()
     {
       return _inputString;
+    }
+
+    public string GetFindString()
+    {
+      return "";
     }
   }
 }
